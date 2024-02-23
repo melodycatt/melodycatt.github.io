0a165b331e20de88e79b851cec930442dac6b216
Melody <edward.lenzner@gmail.com>
2024-02-23 22:18:26 +1100

    im gonna make sure this gets pushed out

--- circle.js
+++ circle.js
@@ -1,33 +1,30 @@
-const centerY = (document.getElementById("center").getBoundingClientRect().top + document.getElementById("center").getBoundingClientRect().bottom) / 2
-const centerX = (document.getElementById("center").getBoundingClientRect().left + document.getElementById("center").getBoundingClientRect().right) / 2
+const centerY = (document.getElementById("circleCenter").getBoundingClientRect().top + document.getElementById("circleCenter").getBoundingClientRect().bottom) / 2
+const centerX = (document.getElementById("circleCenter").getBoundingClientRect().left + document.getElementById("circleCenter").getBoundingClientRect().right) / 2
 const circleSlider = document.getElementById("circleSlider")
-let r = 300
-document.addEventListener("mousemove", (e) => {
+const circleCenter = document.getElementById("circleCenter")
+let r1 = 300
+
+var circleDragging = false;
+circleCenter.addEventListener("mousedown", () => {
+    circleDragging = true
+    document.addEventListener("mousemove", circleMove)
+})
+
+document.addEventListener("mouseup", () => {
+    if (circleDragging) {
+        circleDragging = false
+        document.removeEventListener("mousemove", circleMove)
+    }
+})
+
+
+function circleMove(e) {
     y = e.clientY - centerY;
     x = e.clientX - centerX
-    if(y <= x) {
-        x = Math.abs(Math.sqrt(Math.abs(Math.pow(r, 2)-Math.pow(y, 2))))
-        if (x < 0) {
-            x = -Math.abs(x)
-        }    
-    } else {
-        y = Math.abs(Math.sqrt(Math.abs(Math.pow(r, 2)-Math.pow(x, 2))))
-        if (y < 0) {
-            y = -Math.abs(y)
-        }    
-    }
-    if (x > 300) {
-        x = 300
-    }
-    if (x < -300) {
-        x = -300
-    }
-    if (y > 300) {
-        y = 300
-    }
-    if (y < -300) {
-        y = -300
-    }
-    console.log(centerX, centerY, e.clientX, e.clientY, x, y)
+    let r2 = Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2))
+    let scale = r1 / r2
+    y *= scale
+    x *= scale
+    console.log(Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2)))
     circleSlider.style.translate = `${x}px ${y}px`
-})
+}
